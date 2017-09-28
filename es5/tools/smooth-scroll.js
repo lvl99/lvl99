c8ef06f136a3bb8f3363b9c2e0ce2224c13afd24
Matt Scheurich <matt@lvl99.com>
2017-09-28 16:27:13 +0200

    added previous smooth scroll that I had more recently developed

--- es5/tools/smooth-scroll.js
+++ es5/tools/smooth-scroll.js
@@ -2,50 +2,149 @@
 
 /**
  * LVL99 Smooth Scroll
- * Ensures smooth motion when navigating to an internal anchor point on the page
+ *
+ * Smoothly scroll to internal anchor links on a page.
+ *
+ * ## Usage
+ *
+ * Smooth Scroll needs to be initialised with jQuery and any configured options. During initialisation the script will
+ * apply the behaviours to any applicable anchor links.
+ *
+ * ```
+ *   let SmoothScroll = require('lvl99/es6/tools/smooth-scroll')(jQuery, { bufferTop: 0 })
+ * ```
+ *
+ * You can also initialise the smoothScroll behaviours by calling `smoothScroll.init()`. This will attach the necessary
+ * events on to anchor links.
+ *
+ * You can trigger the scrollTo event by using the custom event `SmoothScroll.scrollTo`, e.g.:
+ *
+ * ```
+ *   $(document).trigger('SmoothScroll.scrollTo', [ scrollToOptions ])
+ * ```
+ *
+ * The `scrollTo` function emits a custom event `SmoothScroll.scrollTo:start` when the action is invoked and
+ * `SmoothScroll.scrollTo:end` when it finishes.
  *
  * @package lvl99
  */
 
-var $ = require('jquery');
+var SmoothScroll = function SmoothScroll($, options) {
+  /**
+   * Load in the settings
+   */
+  var settings = $.extend({
+    // The space between the top of the window and the top of the target
+    bufferTop: 0,
 
-function SmoothScroll() {
-  return {
-    /**
-     * Scroll to a point in the window
-     *
-     * @param {HTMLElement}|{jQueryObject}|{String} point
-     * @param {Function} cb
-     * @param {Number} time
-     */
-    scrollTo: function scrollTo(point, cb, time) {
-      // Get element to scroll too
-      var $elem = $(point);
-      var winScrollTop = $win.scrollTop();
-      var toScrollTop = 0;
-      var diff = void 0;
+    // The speed to scroll the window
+    scrollSpeed: 1000,
 
-      // Try numeric value
-      if ($elem.length === 0) {
-        toScrollTop = parseInt(point, 10);
-      } else {
-        toScrollTop = $elem.eq(0).offset().top;
+    // The distance from top of window to top of target element to trigger scrolling
+    triggerDistance: 400
+  }, options);
+
+  /**
+   * Smoothly scroll to a target
+   *
+   * @param {String|HTMLElement|jQueryObject} target
+   */
+  function scrollTo(target, scrollToOptions) {
+    // Figure out element to scroll to
+    var $target = $(target).not('[data-disable-smooth-scroll]');
+
+    // More than one target, default to first
+    $target = $target.length > 1 ? $target.eq(0) : $target;
+
+    // Does a scroll target exist?
+    if ($target.length === 1) {
+      // Load in per-use settings
+      var scrollToSettings = $.extend({}, settings, scrollToOptions);
+
+      // Get the target's top offset
+      var targetOffsetTop = $target.offset().top;
+
+      // Get current window scrollTop
+      var windowScrollTop = $(window).scrollTop();
+
+      // Support dynamic bufferTop if it is a function
+      var scrollTop = targetOffsetTop - (typeof scrollToSettings.bufferTop === 'function' ? scrollToSettings.bufferTop() : scrollToSettings.bufferTop);
+
+      // Don't trigger the scroll if the distance is within
+      var checkTriggerDistance = Math.abs(windowScrollTop - scrollTop);
+      if (checkTriggerDistance < scrollToSettings.triggerDistance) {
+        return;
       }
 
-      if (toScrollTop !== winScrollTop) {
-        diff = Math.max(toScrollTop, winScrollTop) - Math.min(toScrollTop, winScrollTop);
+      /**
+       * Emit start event
+       *
+       * @event SmoothScroll.scrollTo:start
+       * @param {jQueryObject} $target
+       * @param {Object}
+       */
+      $target.trigger('SmoothScroll.scrollTo:start', [scrollToSettings]);
 
-        if (typeof time === 'undefined') {
-          time = diff * 0.1;
+      // Do the scroll thing
+      $('html, body').animate({
+        scrollTop: scrollTop
+      }, scrollToSettings.scrollSpeed, function () {
+        // Callback after animation
+        // Must change focus!
+        $target.focus();
+
+        /**
+         * Emit end event
+         *
+         * @event SmoothScroll.scrollTo:end
+         * @param {jQueryObject} $target
+         * @param {Object}
+         */
+        $target.trigger('SmoothScroll.scrollTo:end', [scrollToSettings]);
+
+        // Checking if the target was focused
+        if ($target.is(':focus')) {
+          return false;
         }
-
-        $('html, body').animate({
-          scrollTop: toScrollTop + 'px',
-          skipGSAP: true
-        }, time, 'swing', cb);
-      }
+      });
     }
+  }
+
+  /**
+   * Initialise all links on the page with the smoothScroll functionality
+   */
+  function init() {
+    // Attach link behaviours
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]').not('[href="#0"]').click(function (event) {
+      var $a = $(event.target).closest('a');
+      var hash = $a.attr('href').replace(/.*#([^?]+).*/, '#$1');
+      if ($(hash).length > 0) {
+        event.preventDefault();
+        scrollTo(hash);
+      }
+    });
+
+    // Attach custom event to trigger through DOM
+    $(document).on('SmoothScroll.scrollTo', function (event, scrollToOptions) {
+      if (event.target) {
+        scrollTo(event.target, scrollToOptions);
+      }
+    });
+
+    // Check to see if a hash is located in the window.location and scroll to the element
+    if (window.location.hash) {
+      setTimeout(function () {
+        scrollTo(window.location.hash);
+      }, 1000);
+    }
+  }
+
+  return {
+    init: init,
+    scrollTo: scrollTo
   };
-}
+};
 
 module.exports = SmoothScroll;
